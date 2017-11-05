6eaac366de5fc9e8196855bf4f91b03be1cd6fdb
mike <sorokin.michael.96@yandex.ru>
2017-11-05 18:54:40 +0300

    Almost done the 1st lab

--- linux/lab1/script.sh
+++ linux/lab1/script.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
+echo $0
+
 echo "Hello World!!"
+
+if [ $# -gt 0 ]; then
+	case $1 in
+		\/*)
+			if [[ $1 == $HOME* ]]; then
+				if [ -f "$1" ];then
+					cat $1
+				else
+					if [ $# -gt 1 ]; then
+						echo $2 > $1
+					else
+						echo "There's nothing to write to file!!!"
+					fi
+				fi
+			fi
+			;;
+		* )
+                        if [ -f "$1" ];then
+                                        cat $1
+                                else
+                                        if [ $# -gt 1 ]; then
+                                                echo $2 > $1
+                                        else
+                                                echo "There's nothing to write to file!!!"
+                                        fi
+                                fi
+
+			;;
+	esac
+else
+	echo 'Nothing has been entered!!!'
+fi
